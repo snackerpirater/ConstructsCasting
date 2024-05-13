d635f9fd6605748946769134cd589080d17990a1
SnackerPirater <lolbitz2002@gmail.com>
2024-05-13 16:36:49 -0700

    main branch without slime magic- will come later

--- src/main/java/com/snackpirate/constructscasting/entity/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package com.snackpirate.constructscasting.entity;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/com/snackpirate/constructscasting/spells/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package com.snackpirate.constructscasting.spells;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
