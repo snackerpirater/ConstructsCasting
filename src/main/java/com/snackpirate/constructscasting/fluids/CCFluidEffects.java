9c25f7f92ef253db674c61408700f4c87a50d8bc
SnackerPirater <lolbitz2002@gmail.com>
2025-10-13 10:21:33 -0700

    readd fluid effects, SOME of them work in the encyp apparently

--- src/main/java/com/snackpirate/constructscasting/fluids/CCFluidEffects.java
+++ src/main/java/com/snackpirate/constructscasting/fluids/CCFluidEffects.java
@@ -38,26 +38,26 @@ public class CCFluidEffects extends AbstractFluidEffectProvider {
 	protected void addFluids() {
 //		addFluid(CCFluids.arcaneEssence, FluidValues.SIP).addEntityEffect(ADD_MANA);
 //		addFluid(CCFluids.cinderEssence, FluidValues.SIP).addEntityEffect(DEPLETE_MANA);
-		addMetal(CCFluids.moltenArcanium).fireDamage(2f).addEffect(FluidMobEffect.builder().effect(MobEffects.magicEmpowerment.get(), 100, 1), TimeAction.SET);
-		addMetal(CCFluids.moltenExilite).fireDamage(2f).addEffect(FluidMobEffect.builder().effect(MobEffects.magicVulnerability.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.bloodEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.bloodEmpowerment.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.enderEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.enderEmpowerment.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.evocationEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.evocationEmpowerment.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.fireEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.fireEmpowerment.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.holyEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.holyEmpowerment.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.iceEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.iceEmpowerment.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.lightningEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.lightningEmpowerment.get(), 100, 1), TimeAction.SET);
-		addFluid(CCFluids.natureEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.natureEmpowerment.get(), 100, 1), TimeAction.SET);
-		addMetal(CCFluids.moltenArcaneSalvage).addEffect(FluidMobEffect.builder().effect(MobEffects.recoveryEmpowerment.get(), 100, 2), TimeAction.SET);
+//		addMetal(CCFluids.moltenArcanium).fireDamage(2f).addEffect(FluidMobEffect.builder().effect(MobEffects.magicEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addMetal(CCFluids.moltenExilite).fireDamage(2f).addEffect(FluidMobEffect.builder().effect(MobEffects.magicVulnerability.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.bloodEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.bloodEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.enderEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.enderEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.evocationEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.evocationEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.fireEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.fireEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.holyEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.holyEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.iceEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.iceEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.lightningEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.lightningEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addFluid(CCFluids.natureEssence, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.natureEmpowerment.get(), 100, 1), TimeAction.SET);
+//		addMetal(CCFluids.moltenArcaneSalvage).addEffect(FluidMobEffect.builder().effect(MobEffects.recoveryEmpowerment.get(), 100, 2), TimeAction.SET);
 		addFluid(CCFluids.potatoStew, FluidValues.SIP).addEntityEffect(new RestoreHungerFluidEffect(2, 0.48f, false, ItemOutput.fromItem(CCItems.potatoStewBowl.get())));
 		addFluid(CCFluids.poisonousPotatoStew, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffectRegistry.BLIGHT.get(), 100, 1), TimeAction.SET);
 
 		addFluid(CCFluids.squidInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(net.minecraft.world.effect.MobEffects.BLINDNESS, 100, 1), TimeAction.SET);
-		addFluid(CCFluids.commonInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 160, 1), TimeAction.SET);
-		addFluid(CCFluids.uncommonInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 170, 2), TimeAction.SET);
-		addFluid(CCFluids.rareInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 180, 3), TimeAction.SET);
-		addFluid(CCFluids.epicInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 190, 4), TimeAction.SET);
-		addFluid(CCFluids.legendaryInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 200, 5), TimeAction.SET);
+//		addFluid(CCFluids.commonInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 160, 1), TimeAction.SET);
+//		addFluid(CCFluids.uncommonInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 170, 2), TimeAction.SET);
+//		addFluid(CCFluids.rareInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 180, 3), TimeAction.SET);
+//		addFluid(CCFluids.epicInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 190, 4), TimeAction.SET);
+//		addFluid(CCFluids.legendaryInk, FluidValues.SIP).addEffect(FluidMobEffect.builder().effect(MobEffects.inkyImpairment.get(), 200, 5), TimeAction.SET);
 	}
 
 	@Override
