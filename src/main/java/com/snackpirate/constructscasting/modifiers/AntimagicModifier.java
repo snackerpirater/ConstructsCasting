5a9e3ce07b8a1f05c1bf4e26360d54f9a28ce64f
SnackerPirater <lolbitz2002@gmail.com>
2025-11-11 17:24:07 -0800

    exilite bows, stats + trait working

--- src/main/java/com/snackpirate/constructscasting/modifiers/AntimagicModifier.java
+++ src/main/java/com/snackpirate/constructscasting/modifiers/AntimagicModifier.java
@@ -5,8 +5,12 @@ import io.redspace.ironsspellbooks.entity.mobs.MagicSummon;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.phys.EntityHitResult;
 import org.jetbrains.annotations.Nullable;
 import slimeknights.mantle.client.TooltipKey;
 import slimeknights.tconstruct.library.modifiers.Modifier;
@@ -14,18 +18,21 @@ import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.modifiers.hook.combat.MeleeDamageModifierHook;
 import slimeknights.tconstruct.library.modifiers.hook.display.TooltipModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.ranged.ProjectileHitModifierHook;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
+import slimeknights.tconstruct.library.tools.nbt.ModifierNBT;
 import slimeknights.tconstruct.library.utils.Util;
 
 import java.util.List;
 
 
-public class AntimagicModifier extends Modifier implements MeleeDamageModifierHook, TooltipModifierHook {
+public class AntimagicModifier extends Modifier implements MeleeDamageModifierHook, TooltipModifierHook, ProjectileHitModifierHook {
 	@Override
 	protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
-		hookBuilder.addHook(this, ModifierHooks.MELEE_DAMAGE, ModifierHooks.TOOLTIP);
+		hookBuilder.addHook(this, ModifierHooks.MELEE_DAMAGE, ModifierHooks.TOOLTIP, ModifierHooks.PROJECTILE_HIT);
 		super.registerHooks(hookBuilder);
 	}
 
@@ -43,5 +50,13 @@ public class AntimagicModifier extends Modifier implements MeleeDamageModifierHo
 	}
 	private static boolean isMagicUser(LivingEntity target) {
 		return MagicData.getPlayerMagicData(target).isCasting() || target instanceof AbstractSpellCastingMob || target instanceof MagicSummon || MagicData.getPlayerMagicData(target).getMana() > 101;
+	}
+
+	@Override
+	public boolean onProjectileHitEntity(ModifierNBT modifiers, ModDataNBT persistentData, ModifierEntry modifier, Projectile projectile, EntityHitResult hit, @Nullable LivingEntity attacker, @Nullable LivingEntity target) {
+		if (target != null && isMagicUser(target) && projectile instanceof AbstractArrow arrow) {
+			arrow.setBaseDamage(arrow.getBaseDamage() + modifier.getLevel() * 2f);
+		}
+		return false;
 	}
 }
