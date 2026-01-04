847ed2c2ad17d1da19d809e8cd663e0ad8747852
SnackerPirater <lolbitz2002@gmail.com>
2026-01-03 20:17:40 -0800

    fix lang for spellblade requirement message

--- src/main/java/com/snackpirate/constructscasting/CCLang.java
+++ src/main/java/com/snackpirate/constructscasting/CCLang.java
@@ -68,7 +68,7 @@ public class CCLang extends LanguageProvider {
 		addModifier(CCModifiers.CASTING.getId(), "Casting", "Not for fish, unfortunately.", "Allows the tool to cast spells on right click.");
 		addModifier(CCModifiers.SWIFTCASTING, "Swiftcasting", "Run 'n' Gun!", "Allows the user to retain their full movement speed while casting spells.");
 		addModifier(CCModifiers.SPELLBLADE.getId(), "Spellblade", "Strike!", "Hitting an enemy casts the spell imbued on the tool.");
-		add("modifier.constructs_casting.spellblade.requirement", "Requires Imbued to apply.");
+		add("constructs_casting.modifier.spellblade.requirement", "Requires Imbued to apply.");
 
 		add("constructs_casting.modifier.swiftcasting.requirement", "Requires the Casting ability to be applied first.");
 		addModifier(CCModifiers.IMBUED.getId(), "Imbued", "One more spell!", "Allows the tool to be imbued with a spell.");
