be695e8770d1b07e701b62c56cc7f49b8762bc34
SnackerPirater <lolbitz2002@gmail.com>
2026-01-16 11:48:41 -0800

    mithril restores mana when mining block

--- src/main/java/com/snackpirate/constructscasting/CCLang.java
+++ src/main/java/com/snackpirate/constructscasting/CCLang.java
@@ -9,6 +9,7 @@ import com.snackpirate.constructscasting.modifiers.CombustiveModule;
 import com.snackpirate.constructscasting.spells.CCSpells;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraftforge.common.data.LanguageProvider;
 import slimeknights.mantle.registration.object.FluidObject;
 import slimeknights.tconstruct.library.materials.definition.MaterialId;
@@ -31,7 +32,7 @@ public class CCLang extends LanguageProvider {
 		addMaterial(CCMaterials.exilite, "Exilite", "#1 Wizard Hater", "Deals +2 damage per level to summons and entities with magic capabilities beyond average.");
 		addMaterialTraits(CCMaterials.exilite, "Grants +0.5 projectile power per level against summons and entities with magic capabilities beyond average.", "Grants +10% protection per level against spell (NOT magic) damage.");
 		addMaterial(CCMaterials.cosmichalcum, "Cosmichalcum", "Template flavor text", "Template description");
-		addMaterial(CCMaterials.mithril, "Mithril", "The pride of Moria", "Dealing damage has a 25% chance per level to restore mana (amount scales with damage dealt).");
+		addMaterial(CCMaterials.mithril, "Mithril", "The pride of Moria", "Dealing damage and mining blocks has a 25% chance per level to restore mana (amount scales with damage dealt).");
 		addMaterialTraits(CCMaterials.mithril, "Dealing damage has a 25% chance per level to restore mana (amount scales with damage dealt).", "Consumes mana on hit to reduce incoming damage by 4% per level; the amount of mana consumed scales with the damage negated"); //8 levels = 32% total, 12 = 48%
 		addMaterial(CCMaterials.pyrium, "Pyrium", "The might of Tyros himself", "Critical hits have a 50% chance (+25% chance per additional level) to inflict one stack of Immolation on the target; reaching three stacks of Immolation on a target causes a fiery explosion.");
 		addMaterialTraits(CCMaterials.pyrium, "Landing shots has a 50% chance (+25% chance per additional level) to inflict one stack of Immolation on the target; reaching three stacks of Immolation on a target causes a fiery explosion.", "");
@@ -122,7 +123,7 @@ public class CCLang extends LanguageProvider {
 		addModifier(CCModifiers.COMBUSTIVE, "Combustive", "Boom, boom, boom", "Dealing damage can cause fiery explosions");
 		addModifier(CCModifiers.MANA_PROTECTION, "Mana Protection", "Blocks event the most orcish of spears", "Exchanges mana for some protection");
 		add("modifier.constructs_casting.mana_protection.resistance", "Mana Resistance");
-		addModifier(CCModifiers.SORCEROUS, "Sorcerous", "Witches hate this simple trick!", "Dealing damage has a chance to restore mana");
+		addModifier(CCModifiers.SORCEROUS, "Sorcerous", "Witches hate this simple trick!", "Dealing damage and mining blocks has a chance to restore mana");
 
 		addFluid(CCFluids.arcaneEssence, "Arcane Essence", "Probably tastes like blue raspberry; good if you're short on Mana though.");
 		addFluid(CCFluids.fireEssence, "Fire Essence" ,"Subtle notes of sulfur with an ashy aftertaste; Good if you need a little more FIREpower");
--- src/main/java/com/snackpirate/constructscasting/modifiers/ManaOnHitModule.java
+++ src/main/java/com/snackpirate/constructscasting/modifiers/ManaOnHitModule.java
@@ -1,5 +1,6 @@
 package com.snackpirate.constructscasting.modifiers;
 
+import com.snackpirate.constructscasting.ConstructsCasting;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import io.redspace.ironsspellbooks.network.SyncManaPacket;
@@ -15,19 +16,22 @@ import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.modifiers.entity.ProjectileWithPower;
 import slimeknights.tconstruct.library.modifiers.hook.combat.MeleeHitModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.mining.BlockHarvestModifierHook;
 import slimeknights.tconstruct.library.modifiers.hook.ranged.ProjectileHitModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.special.PlantHarvestModifierHook;
 import slimeknights.tconstruct.library.modifiers.modules.ModifierModule;
 import slimeknights.tconstruct.library.module.HookProvider;
 import slimeknights.tconstruct.library.module.ModuleHook;
 import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
+import slimeknights.tconstruct.library.tools.context.ToolHarvestContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
 import slimeknights.tconstruct.library.tools.nbt.ModifierNBT;
 
 import java.util.List;
 
-public record ManaOnHitModule(LevelingValue manaPerDamage, LevelingValue chance) implements ModifierModule, MeleeHitModifierHook, ProjectileHitModifierHook {
-	public static final List<ModuleHook<?>> HOOKS = HookProvider.<ManaOnHitModule>defaultHooks(ModifierHooks.MELEE_HIT, ModifierHooks.PROJECTILE_HIT);
+public record ManaOnHitModule(LevelingValue manaPerDamage, LevelingValue chance) implements ModifierModule, MeleeHitModifierHook, ProjectileHitModifierHook, BlockHarvestModifierHook {
+	public static final List<ModuleHook<?>> HOOKS = HookProvider.<ManaOnHitModule>defaultHooks(ModifierHooks.MELEE_HIT, ModifierHooks.PROJECTILE_HIT, ModifierHooks.BLOCK_HARVEST);
 	public static final RecordLoadable<ManaOnHitModule> LOADER = RecordLoadable.create(
 			LevelingValue.LOADABLE.requiredField("mana_per_damage", ManaOnHitModule::manaPerDamage),
 			LevelingValue.LOADABLE.requiredField("chance", ManaOnHitModule::chance),
@@ -84,5 +88,37 @@ public record ManaOnHitModule(LevelingValue manaPerDamage, LevelingValue chance)
 		if (attacker instanceof ServerPlayer sp) {
 			PacketDistributor.sendToPlayer(sp, new SyncManaPacket(MagicData.getPlayerMagicData(attacker)));
 		}
+	}
+
+	/**
+	 * @param tool      Tool used
+	 * @param modifier  Modifier level
+	 * @param context   Harvest context
+	 * @param harvested Number of blocks harvested
+	 */
+	@Override
+	public void finishHarvest(IToolStackView tool, ModifierEntry modifier, ToolHarvestContext context, int harvested) {
+		LivingEntity miner = context.getPlayer();
+		if (miner == null) return;
+		float ran = miner.getRandom().nextFloat();
+		float chance = this.chance.compute(modifier.getLevel());
+
+		ConstructsCasting.LOGGER.info("destroy speed {}", context.getState().getDestroySpeed(miner.level(), context.getPos()));
+		//stone is 1.5 break speed
+		//current modifier formula is 3 mana/damage
+		//stone should give maybe 20-30 per level since it's a chance
+		//means obsidian gives 900 mana (50 break speed)
+
+		float manaToAdd = manaPerDamage.compute(modifier.getLevel()) //3
+				* (context.getState().getDestroySpeed(miner.level(), context.getPos())*6) // * 1.5 * 6
+				* ((float) miner.getAttributeValue(AttributeRegistry.MANA_REGEN.get())); // * 1~ish
+		manaToAdd = Math.min(manaToAdd, 100); //hardcoded cap to not abuse, mithril is fairly fast mining
+		if (ran < chance) {
+			addManaToAttacker(miner, manaToAdd);
+		}
+		if (chance > 1 && ran < chance-1) {
+			addManaToAttacker(miner, manaToAdd);
+		}
+
 	}
 }
